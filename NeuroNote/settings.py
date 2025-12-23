16afededdf128a820a409569b95b131e94519379
Jaylen McAfee <jaylenmc05@gmail.com>
2025-12-23 12:57:14 -0600

    Put database url in env file

--- NeuroNote/settings.py
+++ NeuroNote/settings.py
@@ -147,10 +147,10 @@ WSGI_APPLICATION = 'NeuroNote.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-REMOVED = REMOVED
-print(f"REMOVED: {REMOVED}")
+DATABASE_URL = os.environ.get('DATABASE_URL')
+print(f"DATABASE_URL: {DATABASE_URL}")
 DATABASES = {
-    "default": dj_database_url.parse(REMOVED, conn_max_age=600)
+    "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
 }
 
 # Password validation
